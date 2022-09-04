1981f35daec54a8f8dfc1da8b74e614fb6f7f850
toyenekan <71904703+toyenekan@users.noreply.github.com>
2022-09-04 14:03:34 -0600

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      {/* Top Section */}
+      <h1 className={styles.h1}>thirdweb Deploy - Custom Staking Contract</h1>
+      <div className={styles.nftBoxGrid}>
+        
+
+        <div
+          className={styles.optionSelectBox}
+          role="button"
+          onClick={() => router.push(`/stake`)}
+        >
+          {/* Staking an NFT */}
+          <img src={`/icons/token.webp`} alt="drop" />
+          <h2 className={styles.selectBoxTitle}>Stake Your NFTs</h2>
+          <p className={styles.selectBoxDescription}>
+            Use the custom staking contract deployed via <b>thirdweb Deploy</b>{" "}
+            to stake your NFTs, and earn tokens from the <b>Token</b> contract.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
